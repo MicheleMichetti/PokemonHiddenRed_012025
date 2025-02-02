1d8026cfd4173fe5bcac242533602c1ef2945532
michelemichetti <michele.michetti91@gmail.com>
2025-02-02 17:53:27 +0100

    Included allegro libraries. Created base for EntityEngine class.

--- engine/include/graphics_utils.hpp
+++ engine/include/graphics_utils.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <cstdint>
-namespace graphics_utils {
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_ttf.h>
+
+
+
+namespace graphics_utils {
 
 struct PixelCoordinates {
     uint16_t x;
