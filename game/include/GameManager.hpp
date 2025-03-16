06ae9b21d2462dc4222933e8efafa818d739412f
michelemichetti <michele.michetti91@gmail.com>
2025-03-16 16:41:17 +0000

    Autoformat code

--- game/include/GameManager.hpp
+++ game/include/GameManager.hpp
@@ -1,42 +1,37 @@
-#include<Map.hpp>
-#include<Player.hpp>
-#include<Scene.hpp>
-#include<BinaryFileManager.hpp>
-
+#include <BinaryFileManager.hpp>
+#include <Map.hpp>
+#include <Player.hpp>
+#include <Scene.hpp>
 
 class GameManager {
+   private:
+    Map* current_map_;
+    Map* buffer_map_;
+    Player player_;
+    Scene scene_ = Scene::getInstance();
+    BinaryFileManager binary_file_manager_;
 
-    private:
-        Map* current_map_;
-        Map* buffer_map_;
-        Player player_;
-        Scene scene_ = Scene::getInstance();
-        BinaryFileManager binary_file_manager_;
+   public:
+    GameManager();
+    GameManager(Map current_map, Player player, BinaryFileManager binary_file_manager);
+    ~GameManager();
 
-    public:
-        GameManager();
-        GameManager(Map current_map, Player player, BinaryFileManager binary_file_manager);
-        ~GameManager();
+    void setCurrentMap(Map current_map);
+    void setBufferMap(Map buffer_map);
+    void setPlayer(Player player);
+    void setScene(/* ... */);
+    void setBinaryFileManager(BinaryFileManager binary_file_manager);
 
-        void setCurrentMap(Map current_map);
-        void setBufferMap(Map buffer_map);
-        void setPlayer(Player player);
-        void setScene(/* ... */);
-        void setBinaryFileManager(BinaryFileManager binary_file_manager);
+    Map* getCurrentMap();
+    Map* getBufferMap();
+    Player getPlayer();
+    Scene getScene();
+    BinaryFileManager getBinaryFileManager();
 
-        Map* getCurrentMap();
-        Map* getBufferMap();
-        Player getPlayer();
-        Scene getScene();
-        BinaryFileManager getBinaryFileManager();
-
-        void retrieveMapFromFile();
-        void retrieveEntitiesFromFile();
-        void retrieveInteractionsFromFile();
-        void retrieveTilesFromFile();
-        void retrieveCurrentPlayerInstance();
-        uint8_t retrieveCommandFromUser();
+    void retrieveMapFromFile();
+    void retrieveEntitiesFromFile();
+    void retrieveInteractionsFromFile();
+    void retrieveTilesFromFile();
+    void retrieveCurrentPlayerInstance();
+    uint8_t retrieveCommandFromUser();
 };
-
-
-
